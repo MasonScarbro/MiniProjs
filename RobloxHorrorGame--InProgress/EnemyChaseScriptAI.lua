9364b285f2fb6f0fbbc22e38e25d993ede056dc9
MasonScarbro <120221711+MasonScarbro@users.noreply.github.com>
2023-06-01 16:02:02 -0400

    Update and rename EnemyChaseScript.lua to EnemyChaseScriptAI.lua

--- RobloxHorrorGame--InProgress/EnemyChaseScriptAI.lua
+++ RobloxHorrorGame--InProgress/EnemyChaseScriptAI.lua
@@ -4,20 +4,37 @@ local players = game:GetService("Players") -- Players will help "get" the player
 local humanoid = script.Parent -- grabs the parent of the script which is humanoid
 local root = humanoid.Parent.PrimaryPart --root is the humanoids parent i.e models primary part which is typically the humanoid root part
 
+local PathfindingService = game:GetService("PathfindingService"); -- A path finding service
+
+
+
 local wantedDistance = 30 --  How far he can search or should be trying to search, The value now is small testing needed
 local stopDistance = 5 -- In caase we want to use this to make him stop (like if he had a kill radius instead of touching)
 
+local damage = 50
+local attackDistance = 8
+local attackWait = 1
+local lastAttack = tick()
+
+
+
+
+
+
 function findNearestPlaya()
 	local playerList = players:GetPlayers()
-	
+
 	local playerNearest = nil
 	local dist = nil
-	local direction = nil
-	
+	local direction = nil	
 	for _, player in pairs(playerList) do -- basically a for each loop that says for each player in the list of players
 		local character = player.Character 
 		if character  then -- will only eggsacute if a player/character exists
 			local distanceV = player.Character.HumanoidRootPart.Position - root.Position -- Distance  ''Vector'' equals the distance from the player torso/Root position minus the distance of the models primary part which we have as root
+
+
+
+
 
 			if not playerNearest then
 				playerNearest = player
@@ -33,25 +50,34 @@ function findNearestPlaya()
 
 			end
 		end
-	
+
 	end
-	
+
 	return playerNearest, dist, direction -- function so return which playa and his distance and direction
 end
 
 -- Another call from the runService class that runs every "physics frame", Which I think is like the updateBefore in unity
 runService.Heartbeat:Connect(function() -- lua thing essentially this odd function call thing is just an anonymous function meaning it will execute every heartbeat
-	local playerNearest, distance, direction = findNearestPlaya()
+
+
+	local playerNearest, distance, direction = findNearestPlaya()	
 	-- if the distance is within range of the wanted distance
-	if playerNearest  then
-		if distance <=wantedDistance and distance >= stopDistance then
-
-			humanoid:Move(direction) -- moves in the direction of the playa
-		else 
-			humanoid:Move(Vector3.new()) -- 
-
+	if playerNearest and distance <= wantedDistance  then
+		local path = PathfindingService:CreatePath(); -- Now if hes in range of the player create a new path
+		path:ComputeAsync(root.Position, playerNearest.Character.HumanoidRootPart.Position) -- compute the path with the positions of the player and Dr. Sturgeon
+		local waypoints = path:GetWaypoints()  -- Assign a new waypoint and and gathers the paths 
+		-- standard for each loop for the nodes SEE DOCUMENTATION/TUTORIALS TO UNDERSTAND ''NODES''
+		for _, waypoint in pairs(waypoints)  do 
+			humanoid:MoveTo(waypoint.Position) -- For each waypoint/node move Dr. sturgeon to it until it reaches the computed path i.e the distance from the player to the robot vice a versa
+			humanoid.MoveToFinished:Wait()
 		end
 	end
 
 
+	if distance <= attackDistance and tick() - lastAttack >= attackWait then
+		lastAttack = tick();
+		playerNearest.Character.Humanoid.Health -= damage
+	end
+
 end)
+		
